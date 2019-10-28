4ec2d6d35ed240f0be967e5d3036188feebf44ea
Catofes <i@catofes.com>
2019-10-28 21:40:06 +0800

    Update config flag

--- src/main.go
+++ src/main.go
@@ -4,7 +4,7 @@ import "flag"
 
 //Run function is the entry of this program
 func Run() {
-	path := flag.String("config", "./config.json", "config path")
+	path := flag.String("c", "./config.json", "config path")
 	flag.Parse()
 	c := (&config{}).load(*path)
 	s := (&tlsServer{config: *c}).init()
